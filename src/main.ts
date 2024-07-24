c4fabe1b2dc3480689e02c220450e2f4e4ddde7e
liusuolong001 <2531134419@qq.com>
2024-07-24 16:50:15 +0800

    vue-18n

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@
  * @Author: liusuolong001
  * @Date: 2024-07-19 22:31:16
  * @LastEditors: liusuolong001
- * @LastEditTime: 2024-07-24 16:46:57
+ * @LastEditTime: 2024-07-24 16:49:47
  */
 
 import { createApp } from 'vue'
@@ -26,14 +26,14 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
 
-const i18n = new VueI18n({
-  locales: ['en', 'de'],
-  messages: {
-    // 你的语言信息
-    // 'zh-CN': require('./lang/zh-CN'),
-    // en: require('./lang/en')
-  }
-})
+// const i18n = new VueI18n({
+//   locales: ['en', 'de'],
+//   messages: {
+//     // 你的语言信息
+//     // 'zh-CN': require('./lang/zh-CN'),
+//     // en: require('./lang/en')
+//   }
+// })
 
 app.use(pinia)
 // 解决页面刷新router都会变成默认的几项 此方法解决动态配置路由不见问题
